53e2964512e24ba74407de3c7c295c145ab58073
Farruco Sanjurjo <madtrick@gmail.com>
2023-12-05 11:22:51 +0100

    Add some notes to some Armeria services

--- todo/WebServer/src/main/scala/com/github/madtrick/Server.scala
+++ todo/WebServer/src/main/scala/com/github/madtrick/Server.scala
@@ -4,6 +4,13 @@ import com.linecorp.armeria.server.annotation.Post
 import com.linecorp.armeria.common.HttpResponse
 import com.linecorp.armeria.server.Server
 import main.scala.TodoItemsCollection
+import com.linecorp.armeria.server.RouteBuilder
+import com.linecorp.armeria.common.HttpMethod
+import com.linecorp.armeria.server.AbstractHttpService
+import com.linecorp.armeria.server.ServiceRequestContext
+import com.linecorp.armeria.common.HttpRequest
+import main.cases.AddTodo
+import com.linecorp.armeria.common.HttpStatus
 
 class SampleService {
   @Post("/hello")
@@ -17,12 +24,32 @@ object Main extends App {
   val builder = Server.builder()
   builder.http(8080)
 
+  /** This code is commented because it behaves as catch all for /todos
+    */
+  // builder.service(
+  //   "/todos",
+  //   (ctx, req) => {
+  //     val todos = TodoItemsCollection.load()
+  //     println("Received request")
+  //     HttpResponse.ofJson(todos)
+  //   }
+  // )
+
+  // This service implentation was abandoned because I couldn't understand
+  // how to get the "CompletableFuture" to work. After calling "join" the
+  // handler will stuck and make no progress
   builder.service(
-    "/",
-    (ctx, req) => {
-      val todos = TodoItemsCollection.load()
-      println("Received request")
-      HttpResponse.ofJson(todos)
+    "/todos_abandoned",
+    new AbstractHttpService() {
+      protected override def doPost(ctx: ServiceRequestContext, req: HttpRequest): HttpResponse = {
+        println("POST received")
+        val completable = req.aggregate()
+        val request     = completable.join()
+        // Code below doesn't run
+        println("Headers", request.headers())
+        println("Body", request.content())
+        HttpResponse.of(HttpStatus.OK)
+      }
     }
   )
 
